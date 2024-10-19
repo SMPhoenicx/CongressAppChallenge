6a5e17ffc3acdb84437fee0dedc03c1edb764387
Suman <sumanmuppavarapu@gmail.com>
2024-10-19 15:22:28 -0500

    Initial Commit

---
+++ CongressAppChallenge/CongressAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CongressAppChallengeApp.swift
+//  CongressAppChallenge
+//
+//  Created by Suman Muppavarapu on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CongressAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CongressAppChallengeTests/CongressAppChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  CongressAppChallengeTests.swift
+//  CongressAppChallengeTests
+//
+//  Created by Suman Muppavarapu on 10/19/24.
+//
+
+import Testing
+@testable import CongressAppChallenge
+
+struct CongressAppChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
